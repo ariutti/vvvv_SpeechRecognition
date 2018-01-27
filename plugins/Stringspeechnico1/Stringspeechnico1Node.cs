76e08b0a71902c9f6346f3e21fa04cd19718f91a
ariutti <nicola.ariutti@gmail.com>
2018-01-27 20:34:05 +0100

    almost done: need to add a reset bang, and a more sophisticated way to deal with output modification

--- plugins/Stringspeechnico1/Stringspeechnico1Node.cs
+++ plugins/Stringspeechnico1/Stringspeechnico1Node.cs
@@ -11,11 +11,10 @@ using VVVV.Core.Logging;
 
 namespace VVVV.Nodes
 {
-
 	/* PLUGIN INFO ********************************************************/
 	[PluginInfo(Name = "speechnico1",
 				Category = "String",
-				Help = "Basic template with one string in/out",
+				Help = "An italian speech recognition plugin",
 				Tags = "c#")]
 
 	/* CLASS **************************************************************/
@@ -23,20 +22,22 @@ namespace VVVV.Nodes
 	{
 		/* Pins definition ************************************************/
 		[Input("Choices", DefaultString = "input strings")]
-		public IDiffSpread<string> FInput;
+		public IDiffSpread<string> InChoices;
 
-		[Input("BBang!", IsBang=true)]
-		public IDiffSpread<bool> FBang;
+		[Input("Confidence Threshold!")]
+		public IDiffSpread<double> InConfidenceThreshold;
 
 		[Output("Guess", DefaultString = "")]
-		public ISpread<string> FOutput;
+		public ISpread<string> OutGuess;
 
 		[Output("Confidence")]
-		public ISpread<double> Confidence;
+		public ISpread<double> OutConfidence;
+		
+		[Output("Completed", IsBang=true)]
+		public ISpread<bool> OutBangCompleted;
 
 		[Import()]
 		public ILogger FLogger;
-
 
 		/* class fields *******************************************************/
 		public RecognizerInfo info = null;
@@ -45,18 +46,18 @@ namespace VVVV.Nodes
 		public Thread th;
 		public int nSlices = 0;
 
-
-    // shared fields between threads
+    	// shared fields between threads
 		public string guess = "";
 		public double confidence = 0.0;
-    static bool done = false;
-    // this locker is used for thread safety
-    static readonly object locker = new object();
+		public bool bCompleted = false;
+    	// this locker is used for thread safety
+   		static readonly object locker = new object();
 
 
 		/* CONSTRUCTOR ********************************************************/
 		public Stringspeechnico1Node()
 		{
+			// maybe there's a better way to initialize the sre language
 			foreach( RecognizerInfo ri in SpeechRecognitionEngine.InstalledRecognizers() ) {
 				if( ri.Culture.TwoLetterISOLanguageName.Equals("it")) {
 					info = ri;
@@ -68,109 +69,139 @@ namespace VVVV.Nodes
 			}
 
 			sre = new SpeechRecognitionEngine( info );
+			
+			// Speech recognition Engine Settings //
 			sre.SetInputToDefaultAudioDevice();
-
-			/* add the event handlers */
-			sre.SpeechRecognized   += new EventHandler<SpeechRecognizedEventArgs>(sr);
-			sre.RecognizeCompleted += new EventHandler<RecognizeCompletedEventArgs>(rc);
-			//sre.SpeechRecognitionRejected += new EventHandler<SpeechRecognitionRejectedEventArgs>(srr);
-
-			th = new Thread( recon );
-			th.Name = "Recon";
-			th.Start();
+			// timing
+			sre.InitialSilenceTimeout = TimeSpan.FromSeconds(3);
+            sre.BabbleTimeout = TimeSpan.FromSeconds(2);
+            sre.EndSilenceTimeout = TimeSpan.FromSeconds(1);
+            sre.EndSilenceTimeoutAmbiguous = TimeSpan.FromSeconds(1.5);
+			// handlers
+            sre.SpeechDetected += new EventHandler<SpeechDetectedEventArgs>(h_detected);
+            sre.SpeechHypothesized += new EventHandler<SpeechHypothesizedEventArgs>(h_hypothesized);
+            sre.SpeechRecognitionRejected += new EventHandler<SpeechRecognitionRejectedEventArgs>(h_rejected);
+            sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(h_recognized);
+            sre.RecognizeCompleted += new EventHandler<RecognizeCompletedEventArgs>(h_completed);
 		}
-
 
 		/* RECON THREAD *******************************************************/
-		public void recon()
-		{
-			while( 1 != 2 )
-			{
-				FLogger.Log( LogType.Debug, "recon: Thread ({0} - {1}): {2}, Priority {3}",
-											Thread.CurrentThread.ManagedThreadId,
-											Thread.CurrentThread.Name,
-											Thread.CurrentThread.ThreadState,
-											Thread.CurrentThread.Priority);
-				sre.Recognize();
-				FLogger.Log( LogType.Debug, "fine del riconoscimento?!");
-			}
-		}
-
-
-		/* CALLBACKS **********************************************************/
-		// speech recognized
-		public void sr(object sender, SpeechRecognizedEventArgs e)
-		{
-			lock(locker)
-			{
-				confidence = e.Result.Confidence;
-				guess = e.Result.Text;
-			}
-			FLogger.Log( LogType.Debug, "Guess: {0}, Confidence {1}.", guess, confidence.ToString() );
-			//th.Join();
-		}
-
-		// recognize complete
-		public void rc(object sender, RecognizeCompletedEventArgs e)
-		{
-			// who is the thread which is running this piece of code
-			FLogger.Log( LogType.Debug, "rc: Thread ({0} - {1}): {2}, Priority {3}",
-											Thread.CurrentThread.ManagedThreadId,
-											Thread.CurrentThread.Name,
-											Thread.CurrentThread.ThreadState,
-											Thread.CurrentThread.Priority);
-			FLogger.Log( LogType.Debug, "completed");
-			//done = true;
+		public void recon() {
+			FLogger.Log( LogType.Debug, "Th ({0} {1}) - recon: {2}, Priority {3}",
+										Thread.CurrentThread.ManagedThreadId,
+										Thread.CurrentThread.Name,
+										Thread.CurrentThread.ThreadState,
+										Thread.CurrentThread.Priority);
+			sre.RecognizeAsync(RecognizeMode.Single);
 		}
 
 		/* EVALUATE ***********************************************************/
 		//called when data for any output pin is requested
-		public void Evaluate(int SpreadMax)
-		{
+		public void Evaluate(int SpreadMax) {
+			
+			if( InConfidenceThreshold.IsChanged ) {
+				var c = (Int32)InConfidenceThreshold[0];
+				if(c > 100)
+					c=100;
+				else if( c < 0)
+					c = 0;
+				//FLogger.Log(LogType.Debug, sre.QueryRecognizerSetting("CFGConfidenceRejectionThreshold").ToString() );
+				sre.UpdateRecognizerSetting("CFGConfidenceRejectionThreshold", c);
+				//FLogger.Log(LogType.Debug, sre.QueryRecognizerSetting("CFGConfidenceRejectionThreshold").ToString() );
+			}
+			
 			// do something only if input is changed
-			if( FInput.IsChanged )
-			{
+			if( InChoices.IsChanged ) {
 				FLogger.Log(LogType.Debug, "## Input changed ##");
-
+				
+				// cancle the current recognition operation (if any)
+				sre.RecognizeAsyncCancel();
+				
+				FLogger.Log(LogType.Debug, "Current Confidence: {0}", sre.QueryRecognizerSetting("CFGConfidenceRejectionThreshold"));
+				
+				// clear the current grammar
+				sre.UnloadAllGrammars();
+				
 				// generate grammar from input strings
 				GrammarBuilder gb = new GrammarBuilder();
 				gb.Culture = sre.RecognizerInfo.Culture;
 
 				// fill the list of alternative words
 				choices = new Choices();
-				for(int i=0; i< SpreadMax; i++)
-				{
-					FLogger.Log(LogType.Debug, "slice: "+FInput[i] );
-					choices.Add( FInput[i] );
+				for(int i=0; i< SpreadMax; i++) {
+					FLogger.Log(LogType.Debug, "  slice: "+InChoices[i] );
+					choices.Add( InChoices[i] );
 				}
 				// and put it inside the Grammar Builder
 				gb.Append(choices);
-				// use the grammar builder the build the grammar
+				// use the grammar builder to build the grammar
 				Grammar g = new Grammar( gb );
 				// load the grammar inside the engine
-				// Beware: access to shared data
-				//lock( locker ) {
-					sre.LoadGrammar( g );
-				//}
-
-				//th = new Thread( recon );
-				//th.Name = "Recon";
-				//th.Start();
+				sre.LoadGrammar( g );
+				
+				FLogger.Log( LogType.Debug, g.ToString());
+				
+				th = new Thread( recon );
+				th.Name = "Recon";
+				th.Start();
 			} // end of if( FInput.IsChanged )
 
+			//FLogger.Log(LogType.Debug, bCompleted.ToString() );
+		
 			// replicate the string for all
-			FOutput[0] = guess;
-			/*
-			for (int i = 0; i < SpreadMax; i++)
-			{
-				// Beware: access to shared data
-				//lock( locker) {
-					FOutput[0] = guess;
-			  //}
-				*/
+            lock(locker) {
+                OutGuess[0] = guess;
+            	OutConfidence[0] = confidence;
+            }
+
+			OutBangCompleted[0] = bCompleted;
+			bCompleted = false;		
+		}
+
+		
+		/* HANDLERS ***********************************************************/
+		public void h_detected(object sender, SpeechDetectedEventArgs e) {
+			FLogger.Log( LogType.Debug, "Th {0} - detected;", Thread.CurrentThread.ManagedThreadId);
+		}
+		
+        public void h_hypothesized(object sender, SpeechHypothesizedEventArgs e) {
+			FLogger.Log( LogType.Debug, "Th {0} - hyp {1} - score {2};", Thread.CurrentThread.ManagedThreadId, 
+                                                    e.Result.Text,
+                                                    e.Result.Confidence);
+		}
+		
+		public void h_rejected(object sender, SpeechRecognitionRejectedEventArgs e) {
+			FLogger.Log( LogType.Debug, "Th {0} - rejected;", Thread.CurrentThread.ManagedThreadId);
+            foreach (RecognizedPhrase phrase in e.Result.Alternates) {
+                FLogger.Log( LogType.Debug, "  Rejected phrase; {0} - score: {1}: ", phrase.Text, phrase.Confidence);
+            }
+		}
+
+		public void h_recognized(object sender, SpeechRecognizedEventArgs e) {
+			
+			lock(locker) {
+				confidence = e.Result.Confidence;
+				guess = e.Result.Text;
 			}
+			FLogger.Log( LogType.Debug, "Th {0} - recon {1} - {2};", 
+                                            Thread.CurrentThread.ManagedThreadId, 
+                                            e.Result.Text,
+                                            e.Result.Confidence);
+		}
 
-
-		} // end of Evaluate(int SpreadMax)
+		public void h_completed(object sender, RecognizeCompletedEventArgs e) {		
+			if( e.Result != null ) {
+                FLogger.Log( LogType.Debug, "Th {0} - completed '{1}' - {2};", 
+											Thread.CurrentThread.ManagedThreadId, 
+											e.Result.Text,
+                                            e.Result.Confidence);
+				bCompleted = true;				
+            } else {
+                FLogger.Log( LogType.Debug, "Th {0} - completed;", 
+            								Thread.CurrentThread.ManagedThreadId);
+            }
+				
+			FLogger.Log( LogType.Debug, "");
+		}
 	}
 }
